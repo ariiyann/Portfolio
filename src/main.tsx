be9c76dfee27f3c41731ce2758f677adea387022
ariiyann <116436190+ariiyann@users.noreply.github.com>
2025-06-16 12:07:20 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+//import React, { useEffect, useState, useRef } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
